47bc4244099d1f74fdb8b1e8bb1eec1175e96918
vincent <scrapp3rz@gmail.com>
2018-04-30 19:30:29 +0200

    design profileHeader ok

--- AppMyDelight/View/CollectionViewCell/ProfileHeader.swift
+++ AppMyDelight/View/CollectionViewCell/ProfileHeader.swift
@@ -15,10 +15,39 @@ let PROFILE_HEADER = "ProfileHeader"
 class ProfileHeader: UICollectionReusableView {
 
     
+    @IBOutlet weak var Image_Fond: UIImageView!
+    @IBOutlet weak var Profile_Image: roundedImage!
+    @IBOutlet weak var Followers_Label: UILabel!
+    @IBOutlet weak var Following_Label: UILabel!
+    @IBOutlet weak var Username_Label: UILabel!
+    @IBOutlet weak var Button_Follow_Or_Change: MydelButton!
+    @IBOutlet weak var TextView: UITextView!
+    @IBOutlet weak var Segmented: UISegmentedControl!
+    
+    
+    
+    
+    
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    
+    @IBAction func Button_Switched_On(_ sender: Any) {
+    }
+    
+    @IBAction func Segment_Choosen(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
