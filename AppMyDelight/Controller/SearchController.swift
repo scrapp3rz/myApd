eeabad2710bb527d9ad60a418372dae41dbe00d6
vincent <scrapp3rz@gmail.com>
2018-05-02 14:10:05 +0200

    affichage des # et des username en tableview

--- AppMyDelight/Controller/SearchController.swift
+++ AppMyDelight/Controller/SearchController.swift
@@ -20,37 +20,56 @@ class SearchController: UIViewController, UITableViewDelegate, UITableViewDataSo
     var hashtags = [Hashtag]()
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         TableView.delegate = self
         TableView.dataSource = self
+        let userNib = UINib(nibName: USER_CELL, bundle: nil)
+        TableView.register(userNib, forCellReuseIdentifier: USER_CELL)
+        let hashNib = UINib(nibName: HASHTAG_CELL, bundle: nil)
+        TableView.register(hashNib, forCellReuseIdentifier: HASHTAG_CELL)
+
         getHashtagFromBDD()
         getUserFromBDD()
-
     }
 
     func getUserFromBDD() {
         BDD().getAllUsers { (util) -> (Void) in
             if let user = util {
                 self.users.append(user)
+                self.TableView.reloadData()
             }
         }
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        if Segment.selectedSegmentIndex == 0 {
+            return users.count
+        } else {
+            return hashtags.count
+        }
     }
 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        if Segment.selectedSegmentIndex == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: USER_CELL) as! UserCell
+            cell.setup(user: users[indexPath.row])
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: HASHTAG_CELL) as! HashtagCell
+            cell.setup(hashtag: hashtags[indexPath.row])
+            return cell
+        }
     }
 
     func getHashtagFromBDD() {
         BDD().getHashtags { (hash) -> (Void) in
             if let hashtag = hash {
                 self.hashtags.append(hashtag)
+                self.TableView.reloadData()
                 print(hashtag.name)
             }
         }
@@ -58,5 +77,16 @@ class SearchController: UIViewController, UITableViewDelegate, UITableViewDataSo
     
 
     @IBAction func Segment_Choosen(_ sender: Any) {
+            TableView.reloadData()
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if Segment.selectedSegmentIndex == 0 {
+            return 80
+        } else {
+            return 50
+        }
+    }
+    
+    
 }
--- AppMyDelight/View/TableViewCell/HashtagCell.swift
+++ AppMyDelight/View/TableViewCell/HashtagCell.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
+let HASHTAG_CELL = "HashtagCell"
+
+
+
+
+
 class HashtagCell: UITableViewCell {
 
+    
+    @IBOutlet weak var Text_Hashtag: UILabel!
+    
+    var hashtag: Hashtag!
+    
+    
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,5 +35,20 @@ class HashtagCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    
+    func setup(hashtag: Hashtag) {
+        self.hashtag = hashtag
+        var string2: String = "\n"
+        if self.hashtag.posts.count <= 1 {
+            string2 += String(self.hashtag.posts.count) + " " + " mention"
+        } else {
+            string2 += String(self.hashtag.posts.count) + " " + " mentions"
+        }
+        Text_Hashtag.attributedText(string1: self.hashtag.name, string2: string2)
+    }
+    
+    
+    
     
 }
